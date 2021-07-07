bca1f5fa00821cdbf17aba0880ad0e91d7194fc2
luke-scot <lshc3@cam.ac.uk>
2021-07-07 16:18:13 +0000

    First thermal stitching

--- download_data.sh
+++ download_data.sh
@@ -8,9 +8,9 @@ mkdir data
 cd data
 
 # Library, Sidgwick, Maths data
-gdown -O lsm.zip "https://drive.google.com/uc?export=download&id=1OP-2m0nApOUYqW3knGbe7-ey6Ke_hPJM"
-
+gdown -O lsm.zip "https://drive.google.com/uc?export=download&id=1e0Ug-O_bD2hhu6yweiRpl1fqdyA65JFG"
 unzip lsm.zip -d lsm
+rm lsm.zip
 
 echo "Downloaded Library/Sidgwick/Maths data."
 
@@ -19,9 +19,11 @@ read -p "Do you wish to download all other data? (Type Y/N)" y
 if [ "$y" = "Y" ]; then
   gdown -O west.zip "https://drive.google.com/uc?export=download&id=1AXoHWmgDuckD4u-SJJN9HXp_6xEdao1U"
   unzip west.zip -d west
+  rm west.zip
   
   gdown -O centre.zip "https://drive.google.com/uc?export=download&id=1Cyz7CQDlcCEcRXyxF6QhJUz0lutj2u-2"
   unzip centre.zip -d centre
+  rm centre.zip
 fi
 
 echo "Finished Downloading!"
